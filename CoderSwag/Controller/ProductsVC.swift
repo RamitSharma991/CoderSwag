37d96ba9c332bc2ccfdb99d61ed70b37c69fc6d3
RamitSharma991 <RamitsMac@Ramits-MacBook-Pro.local>
2017-11-28 14:30:47 +0530

    test commit

--- CoderSwag/Controller/ProductsVC.swift
+++ CoderSwag/Controller/ProductsVC.swift
@@ -8,28 +8,39 @@
 
 import UIKit
 
-class ProductsVC: UIViewController {
+class ProductsVC: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
 
+    @IBOutlet weak var productsCollection: UICollectionView!
+    
+    private(set) public var products = [Product]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        productsCollection.delegate = self
+        productsCollection.dataSource = self
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func initProducts(category: Category) {
+        
+        products = DataService.instance.getProducts(forCategoryTitle: category.title)
+        navigationItem.title = category.title
+        
+    }
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return products.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        if let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "ProctCell", for: indexPath) as? ProductCell {
+            
+            let product = products[indexPath.row]
+            cell.updateViews(product: product)
+            return cell
+        }
+        
+        return ProductCell()
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
--- CoderSwag/View/ProductCell.swift
+++ CoderSwag/View/ProductCell.swift
@@ -12,5 +12,12 @@ class ProductCell: UICollectionViewCell {
     
     @IBOutlet weak var productImageView : UIImageView!
     @IBOutlet weak var productTitle: UILabel!
-    @IBOutlet weak var productPrice : UILabel!    
+    @IBOutlet weak var productPrice : UILabel!
+    
+    func updateViews(product: Product) {
+        
+        productImageView.image = UIImage(named: product.imageName)
+        productTitle.text = product.title
+        productPrice.text = product.price
+    }
 }
